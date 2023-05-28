f9554af0c383387be2cb3399ed437217e6a7265f
lnvh <ps2ud0@gmail.com>
2023-05-28 20:31:47 +0900

    230528 optimizer 0.0005 -> 0.0002 scaled_data rescaling

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ stock['Year'] = stock.index.year
 stock['Month'] = stock.index.month
 stock['Day'] = stock.index.day
 
+# 스케일링 이전의 원본 데이터 저장
+original_data = stock['Close'].values
+
 stock.head()
 
 plt.figure(figsize=(16, 9))
@@ -28,26 +31,49 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 # 스케일을 적용할 column을 정의합니다.
 scale_cols = ['Open', 'High', 'Low', 'Close', 'Volume']
-# 스케일 후 columns
-scaled = scaler.fit_transform(stock[scale_cols])
 
-df = pd.DataFrame(scaled, columns=scale_cols)
+scale_ft_cols = ['Open','High','Low','Volume']
+
+# 스케일 후 columns
+#scaled = scaler.fit_transform(stock[scale_cols])
+
+scaled_ft = scaler.fit_transform(stock[scale_ft_cols])
+
+#df = pd.DataFrame(scaled, columns=scale_cols)
+df = pd.DataFrame(scaled_ft, columns = scale_ft_cols)
+
+
+scaler1 = MinMaxScaler()
+
+scaled_tg = scaler1.fit_transform(stock['Close'].values.reshape(-1, 1))
+df['Close'] = scaled_tg
+df.index = stock.index
+
+print(df)
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(df.drop(labels = 'Close', axis = 1), df['Close'], test_size=0.2, random_state=0, shuffle=False)
+x_train, x_test, y_train, y_test = train_test_split(df.drop(labels = 'Close', axis = 1), df['Close'], test_size=0.3, random_state=0, shuffle=False)
 
+# 테스트 데이터에 대한 스케일링 이전의 원본 데이터 추출
 WINDOW_SIZE=20
 BATCH_SIZE=32
 
 def windowed_dataset(series, window_size, batch_size, shuffle):
+    #데이터 차원 확장
     series = tf.expand_dims(series, axis=-1)
+    #텐서 슬라이싱
     ds = tf.data.Dataset.from_tensor_slices(series)
+    #윈도우 생성
     ds = ds.window(window_size + 1, shift=1, drop_remainder=True)
+    #데이터 평탄화 (윈도우 -> 배치)
     ds = ds.flat_map(lambda w: w.batch(window_size + 1))
+    #셔플
     if shuffle:
         ds = ds.shuffle(1000)
+    #입력, 출력 변환
     ds = ds.map(lambda w: (w[:-1], w[-1]))
+    #데이터 셋 배치 단위로 나누기, 버퍼크기 설정 = 1
     return ds.batch(batch_size).prefetch(1)
 
 # train_data는 학습용 데이터셋, test_data는 검증용 데이터셋 입니다.
@@ -62,23 +88,38 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 model = Sequential([
     # 1차원 feature map 생성
-    Conv1D(filters=32, kernel_size=5,
-           padding="causal",
-           activation="relu",
-           input_shape=[WINDOW_SIZE, 1]),
-    # LSTM
+    # 특성 추출 레이어
+    Conv1D
+    (
+        #필터 수, 필터의 크기
+        filters=32, kernel_size=5,
+        #현재위치 이전 정보만 사용하도록 제한
+        padding="causal",
+        #활성화 함수 = relu
+        activation="relu",
+        #입력 데이터 형태
+        input_shape=[WINDOW_SIZE, 1]
+    ),
+    # LSTM, 활성화 함수 tanh, 유닛 16개
     LSTM(16, activation='tanh'),
+    #완전 연결 레이어, 최종 예측
     Dense(16, activation="relu"),
+    #출력 레이어
     Dense(1),
 ])
 
 # Sequence 학습에 비교적 좋은 퍼포먼스를 내는 Huber()를 사용합니다.
+# 손실함수
 loss = Huber()
-optimizer = Adam(0.0005)
+# 최적화 함수
+optimizer = Adam(0.0003)
+# 모델 컴파일, 손실함수 = huber, 최적화함수 = adam, 평가지표 = MSE
 model.compile(loss=Huber(), optimizer=optimizer, metrics=['mse'])
 
 # earlystopping은 10번 epoch통안 val_loss 개선이 없다면 학습을 멈춥니다.
+# val_loss = mse
 earlystopping = EarlyStopping(monitor='val_loss', patience=10)
+
 # val_loss 기준 체크포인터도 생성합니다.
 filename = os.path.join('tmp', 'ckeckpointer.ckpt')
 checkpoint = ModelCheckpoint(filename,
@@ -86,18 +127,31 @@ checkpoint = ModelCheckpoint(filename,
                              save_best_only=True,
                              monitor='val_loss',
                              verbose=1)
-
-history = model.fit(train_data,
-                    validation_data=(test_data),
-                    epochs=50,
-                    callbacks=[checkpoint, earlystopping])
+# 모델 훈련
+history = model.fit\
+    (
+    train_data,
+    validation_data=(test_data),
+    epochs=50,
+    callbacks=[checkpoint, earlystopping]
+    )
 
 model.load_weights(filename)
 
 pred = model.predict(test_data)
 
+rescaled_actual = scaler1.inverse_transform(y_test[WINDOW_SIZE:].values.reshape(-1,1))
+rescaled_actual = pd.DataFrame(rescaled_actual, index=y_test.index[WINDOW_SIZE:])
+rescaled_actual.index = y_test.index[WINDOW_SIZE:]
+
+rescaled_pred = scaler1.inverse_transform(np.array(pred).reshape(-1,1))
+rescaled_pred = pd.DataFrame(rescaled_pred, index=y_test.index[WINDOW_SIZE:])
+
+#
+print(rescaled_pred.iloc[-1])
+
 plt.figure(figsize=(12, 9))
-plt.plot(np.asarray(y_test)[20:], label='actual')
-plt.plot(pred, label='prediction')
+plt.plot(rescaled_actual, label='actual')
+plt.plot(rescaled_pred, label='prediction')
 plt.legend()
 plt.show()
